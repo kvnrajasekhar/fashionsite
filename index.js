8c6e37293a13d3e7dd843738fccd75166b1ca201
KANAGALA VENKATA NAGA RAJASEKHAR <109467292+kvnrajasekhar@users.noreply.github.com>
2023-05-25 22:34:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+$('.shop-btn').on("click", shopnow);
+function shopnow() {
+    cnf = confirm("Do you like to Shop On our site");
+    if (cnf === true) {
+        window.open('#featured-products');
+    }
+}
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
